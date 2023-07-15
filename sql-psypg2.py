aaa75427a5d690d617b8387f7dfe4aa48ae44f90
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-15 17:14:17 +0000

    modifies py file

--- sql-psypg2.py
+++ sql-psypg2.py
@@ -4,22 +4,20 @@ import psycopg2
 # connect to "chinook" database
 connection = psycopg2.connect(database="chinook")
 
-
 # build a cursoe object of the database
 cursor = connection.cursor()
 
+# Q1 - select all records from the "Artist" Table
+cursor.execute('SELECT * FROM "Artist"')
 
 # fetch the results (multiple)
 results = cursor.fetchall()
 
-
 # fetch the result (single)
 # result = cursor.fetchone()
 
-
 # close the connection
 connection.close()
-
 
 # print results
 for result in results:
