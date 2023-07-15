b73785ca01dce78ee6068f1414678fd10eb51dfe
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-15 17:29:41 +0000

    adds new data

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -8,17 +8,17 @@ connection = psycopg2.connect(database="chinook")
 cursor = connection.cursor()
 
 # Q1 - select all records from the "Artist" Table
-cursor.execute('SELECT * FROM "Artist"')
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Aerosmith"])
 
 # fetch the results (multiple)
-results = cursor.fetchall()
+# results = cursor.fetchall()
 
 # fetch the result (single)
-# result = cursor.fetchone()
+result = cursor.fetchone()
 
 # close the connection
 connection.close()
 
 # print results
-for result in results:
-    print(result)
+# for result in results:
+print(result)
